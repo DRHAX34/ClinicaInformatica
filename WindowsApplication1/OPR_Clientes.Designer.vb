f38a8950a45287bbeed2fc7a6aa85b4384f4b2f3
Fycodde <blackdragon_414@hotmail.com>
2014-04-30 21:32:08 +1300

    Add OPR_Clientes Form
    Adiciona a winform dos clientes

---
+++ WindowsApplication1/OPR_Clientes.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class OPR_Clientes
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.nomebox = New System.Windows.Forms.TextBox()
+        Me.moradabox = New System.Windows.Forms.TextBox()
+        Me.codpostalbox = New System.Windows.Forms.TextBox()
+        Me.emailbox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.RadButton5 = New Telerik.WinControls.UI.RadButton()
+        Me.RadButton1 = New Telerik.WinControls.UI.RadButton()
+        Me.RadButton2 = New Telerik.WinControls.UI.RadButton()
+        Me.RadButton3 = New Telerik.WinControls.UI.RadButton()
+        Me.RadButton4 = New Telerik.WinControls.UI.RadButton()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        CType(Me.RadButton5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButton1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButton2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButton3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButton4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(26, 289)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(109, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Cod_Postal"
+        '
+        'nomebox
+        '
+        Me.nomebox.Location = New System.Drawing.Point(204, 136)
+        Me.nomebox.Name = "nomebox"
+        Me.nomebox.Size = New System.Drawing.Size(275, 20)
+        Me.nomebox.TabIndex = 6
+        '
+        'moradabox
+        '
+        Me.moradabox.Location = New System.Drawing.Point(204, 193)
+        Me.moradabox.Name = "moradabox"
+        Me.moradabox.Size = New System.Drawing.Size(275, 20)
+        Me.moradabox.TabIndex = 7
+        '
+        'codpostalbox
+        '
+        Me.codpostalbox.Location = New System.Drawing.Point(204, 293)
+        Me.codpostalbox.Name = "codpostalbox"
+        Me.codpostalbox.Size = New System.Drawing.Size(61, 20)
+        Me.codpostalbox.TabIndex = 8
+        '
+        'emailbox
+        '
+        Me.emailbox.Location = New System.Drawing.Point(204, 240)
+        Me.emailbox.Name = "emailbox"
+        Me.emailbox.Size = New System.Drawing.Size(275, 20)
+        Me.emailbox.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(24, 132)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 25)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Nome"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(24, 180)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 25)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Morada"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(26, 236)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 25)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Email"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(287, 293)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox1.TabIndex = 13
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(271, 296)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(10, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "-"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(13, 13)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(260, 86)
+        Me.Label6.TabIndex = 20
+        Me.Label6.Text = "Clientes"
+        '
+        'RadButton5
+        '
+        Me.RadButton5.Location = New System.Drawing.Point(21, 433)
+        Me.RadButton5.Name = "RadButton5"
+        Me.RadButton5.Size = New System.Drawing.Size(75, 63)
+        Me.RadButton5.TabIndex = 21
+        Me.RadButton5.Text = "Adicionar"
+        '
+        'RadButton1
+        '
+        Me.RadButton1.Location = New System.Drawing.Point(117, 433)
+        Me.RadButton1.Name = "RadButton1"
+        Me.RadButton1.Size = New System.Drawing.Size(75, 63)
+        Me.RadButton1.TabIndex = 22
+        Me.RadButton1.Text = "Editar"
+        '
+        'RadButton2
+        '
+        Me.RadButton2.Location = New System.Drawing.Point(212, 433)
+        Me.RadButton2.Name = "RadButton2"
+        Me.RadButton2.Size = New System.Drawing.Size(75, 63)
+        Me.RadButton2.TabIndex = 23
+        Me.RadButton2.Text = "Remover"
+        '
+        'RadButton3
+        '
+        Me.RadButton3.Location = New System.Drawing.Point(307, 433)
+        Me.RadButton3.Name = "RadButton3"
+        Me.RadButton3.Size = New System.Drawing.Size(75, 63)
+        Me.RadButton3.TabIndex = 24
+        Me.RadButton3.Text = "Restaurar Dados Originais"
+        Me.RadButton3.TextWrap = True
+        '
+        'RadButton4
+        '
+        Me.RadButton4.Location = New System.Drawing.Point(404, 433)
+        Me.RadButton4.Name = "RadButton4"
+        Me.RadButton4.Size = New System.Drawing.Size(75, 63)
+        Me.RadButton4.TabIndex = 25
+        Me.RadButton4.Text = "Cancelar"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(26, 359)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 25)
+        Me.Label7.TabIndex = 26
+        Me.Label7.Text = "Aluno"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(204, 349)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(43, 17)
+        Me.CheckBox1.TabIndex = 27
+        Me.CheckBox1.Text = "Sim"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(204, 383)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(46, 17)
+        Me.CheckBox2.TabIndex = 28
+        Me.CheckBox2.Text = "Não"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(265, 347)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(171, 20)
+        Me.TextBox2.TabIndex = 29
+        '
+        'OPR_Clientes
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(191, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(508, 508)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.RadButton4)
+        Me.Controls.Add(Me.RadButton3)
+        Me.Controls.Add(Me.RadButton2)
+        Me.Controls.Add(Me.RadButton1)
+        Me.Controls.Add(Me.RadButton5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.emailbox)
+        Me.Controls.Add(Me.codpostalbox)
+        Me.Controls.Add(Me.moradabox)
+        Me.Controls.Add(Me.nomebox)
+        Me.Controls.Add(Me.Label4)
+        Me.Name = "OPR_Clientes"
+        Me.Text = "Operações Clientes"
+        CType(Me.RadButton5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButton1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButton2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButton3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButton4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents nomebox As System.Windows.Forms.TextBox
+    Friend WithEvents moradabox As System.Windows.Forms.TextBox
+    Friend WithEvents codpostalbox As System.Windows.Forms.TextBox
+    Friend WithEvents emailbox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents RadButton5 As Telerik.WinControls.UI.RadButton
+    Friend WithEvents RadButton1 As Telerik.WinControls.UI.RadButton
+    Friend WithEvents RadButton2 As Telerik.WinControls.UI.RadButton
+    Friend WithEvents RadButton3 As Telerik.WinControls.UI.RadButton
+    Friend WithEvents RadButton4 As Telerik.WinControls.UI.RadButton
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+End Class
---
+++ WindowsApplication1/OPR_Clientes.vb
@@ -0,0 +1,23 @@
+﻿Public Class OPR_Clientes
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            CheckBox2.Checked = False
+            TextBox2.Enabled = True
+            TextBox2.Show()
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            CheckBox1.Checked = False
+            TextBox2.Enabled = False
+            TextBox2.Hide()
+        End If
+    End Sub
+
+    Private Sub OPR_Clientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox2.Hide()
+        CheckBox2.Checked = True
+    End Sub
+End Class
